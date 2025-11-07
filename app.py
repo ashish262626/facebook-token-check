43d9a9730e0a205eb4baac44d77eaa909592d545
Server <165246562+ashish262626@users.noreply.github.com>
2025-11-07 12:08:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,6 @@ def check_token():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, if __name__ == '__main__':
+    
     # run app in debug mode on port 5000
     app.run(debug=True, port=5000, host='0.0.0.0')
